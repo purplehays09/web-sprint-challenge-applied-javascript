b38cb3393b1330ddc346c940fb50add2644e30f0
purplehays09 <purplehays527@gmail.com>
2020-08-07 11:13:04 -0600

    I struggled for a while on the dubugger but I finally got it working!

--- components/Cards.js
+++ components/Cards.js
@@ -21,19 +21,62 @@
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
 
+const { default: Axios } = require("axios");
+
+const cardContainter = document.querySelector('.cards-container')
+
+
+Axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(response => {
+        const articlesObj = response.data.articles
+        // const bootstrap = articlesObj.bootstrap
+    
+        for(let subject in articlesObj){
+            console.log(subject)
+            console.log(articlesObj)
+            console.log(articlesObj[subject])
+            articlesObj[subject].forEach(article => {
+                console.log(article)
+                cardContainter.appendChild(cardMaker(article))
+            });
+        }
+        // const newDiv = cardMaker(bootstrap[0])
+        // cardContainter.appendChild(newDiv)
+    })
+    .catch(error => console.log(error))
 
 function cardMaker(cardObj){
     // createElements
-    
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    const name = document.createElement('span');
 
     // classList.add() and textContent.add and src/href/etc...
+    card.classList.add('card');
+    headline.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgDiv.classList.add('img-container');
 
+    headline.textContent = cardObj.headline;
+    name.textContent = cardObj.authorName;
+
+    img.src = cardObj.authorPhoto;
 
     // arrange with appendChild
-
+    card.appendChild(headline)
+    card.appendChild(authorDiv)
+    authorDiv.appendChild(imgDiv)
+    authorDiv.appendChild(name)
+    imgDiv.appendChild(img)
 
     // addEventListeners
+    card.addEventListener('click', event => console.log(cardObj.headline))
 
 
     // return the div
+    console.log(card)
+    return card
 }
